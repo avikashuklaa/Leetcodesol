df73adea42de976d28de3d1bcc2dc09c4c6b14a2
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-06-24 18:08:48 +0530

    Time: 2 ms (53.90%), Space: 43.5 MB (39.67%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    // public void swap(int a, int b){
+    //     int temp=a;
+    //     a=b;
+    //     b=temp;
+    // }
+    public int maxProduct(int[] nums) {
+        int minsf=nums[0];
+        int maxsf=nums[0];
+        int ans=nums[0];
+        int max=nums[0];
+        int min=nums[0];
+        int n=nums.length;
+        
+        for(int i=1; i<n; i++){
+            if(nums[i]<0){
+                max=Math.max(nums[i], minsf*nums[i]);
+                min=Math.min(nums[i], maxsf*nums[i]);
+            }
+            else{
+                max=Math.max(nums[i], maxsf*nums[i]);
+                min=Math.min(nums[i], minsf*nums[i]);
+            }
+            maxsf=max;
+            minsf=min;
+            ans=Math.max(ans, maxsf);
+        }
+        return ans;
+    }
+}
