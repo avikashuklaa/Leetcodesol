200ab885220429be4caa5c262e72e785c72da2b1
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-06-30 22:26:42 +0530

    Time: 2 ms (59.77%), Space: 46.6 MB (66.64%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        ans[0]=0;
+        if(n==0){
+            return ans;
+        }
+        
+        ans[1]=1;
+        
+        for(int i=2; i<n+1; i++){
+            if(i%2==0){
+                ans[i]=ans[i/2];
+            }
+            else{
+                ans[i]=ans[i/2]+1;
+            }
+        }
+        return ans;
+    }
+}
