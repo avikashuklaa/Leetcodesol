41ed6efbf6951bd710daee5b7097b92deea7794c
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-11 01:02:57 +0530

    Time: 1 ms (84.96%), Space: 41 MB (18.42%) - LeetHub

---
+++ 0593-valid-square/0593-valid-square.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        
+        Set<Integer> set = new HashSet<>();
+        set.add(dist(p1, p3));
+        set.add(dist(p2, p4));
+             
+        set.add(dist(p1, p2));
+        set.add(dist(p1, p4));
+        set.add(dist(p2, p3));
+        set.add(dist(p3, p4));
+        
+        if(!set.contains(0) && set.size()==2){
+            return true;
+        }
+        
+        return false;
+       
+    }
+    
+    public int dist(int[] a, int[] b){
+        
+               
+        return (a[0] - b[0])*(a[0] - b[0]) + (a[1] - b[1])*(a[1] - b[1]);
+    }
+}
