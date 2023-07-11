8bf9b200e7a9cbbacd6a505b0ba611b2a35ffa6d
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-11 23:33:30 +0530

    Time: 250 ms (8.65%), Space: 58.5 MB (23.08%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.java
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.java
@@ -1,60 +1,64 @@
 class Solution {
     long count=0;
     
-    public void check(List<Integer> list, int start, int mid, int end, int diff){
+    public void check(List<Integer> ans, int start, int mid, int end, int diff){
         
         int l=start;
         int r=mid+1;
         
         while(l<=mid && r<=end){
-            if(list.get(l) <= (list.get(r) + diff)){
+            if(ans.get(l) <= (ans.get(r) + diff)){
                 count += (end-r+1);
                 l++;
             }
             else{
                 r++;
             }
-            
         }
         
-        List<Integer> num = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        
         for(int i=start; i<=end; i++){
-            num.add(list.get(i));
+            list.add(ans.get(i));
         }
         
-        Collections.sort(num);
+        Collections.sort(list);
         
         for(int i=start; i<=end; i++){
-            list.set(i, num.get(i-start));
+            ans.set(i, list.get(i-start));
         }
         
         return;
     }
     
-    public void mergesort(List<Integer> list, int start, int end, int diff){
+    public void mergesort(List<Integer> ans, int start, int end, int diff){
         
         if(start==end){
             return;
         }
+        
         int mid=(start+end)/2;
         
-        mergesort(list, start, mid, diff);
-        mergesort(list, mid+1, end, diff);
+        mergesort(ans, start, mid, diff);
+        mergesort(ans, mid+1, end, diff);
         
-        check(list, start, mid, end, diff);
+        check(ans, start, mid, end, diff);
         
         return;
-        
     }
+    
     public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
-        count=0;
-        List<Integer> list = new ArrayList<>();
         
-        for(int i=0; i<nums1.length; i++){
-            list.add(nums1[i]-nums2[i]);
+        count=0;
+        
+        List<Integer> ans = new ArrayList<>();
+        int n=nums1.length;
+        
+        for(int i=0; i<n; i++){
+            ans.add(nums1[i]-nums2[i]);
         }
         
-        mergesort(list, 0, nums1.length-1, diff);
+        mergesort(ans, 0, n-1, diff);
         
         return count;
     }
