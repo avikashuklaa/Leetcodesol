0080e7b0d53bf691f7b9b98ddfde38fa536461d0
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-13 02:26:50 +0530

    Time: 0 ms (100.00%), Space: 40 MB (90.92%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         
-        List<List<Integer>> ans=new ArrayList<>();
-        ArrayList<Integer> curr=new ArrayList<>();
-        find(1, 9, ans, curr, k, n);
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        find(ans, new ArrayList<>(), 1, 9, k, n);
         return ans;
+        
     }
     
-    public void find(int start, int end, List<List<Integer>> ans, ArrayList<Integer> curr, int k, int n){
+    public void find(List<List<Integer>> ans, List<Integer> curr, int start, int end, int k, int target){
         
-        if(n==0 && curr.size()==k){
-            ans.add(new ArrayList(curr));
+        if(target == 0 && curr.size() == k){
+            ans.add(new ArrayList<>(curr));
         }
-        
-        for(int i=start; i<=end; i++){
-            curr.add(i);
-            find(i+1, end, ans, curr, k, n-i);
-            curr.remove(curr.size()-1);
+        else{
+            for(int i=start; i<=end; i++){
+                curr.add(i);
+                find(ans, curr, i+1, end, k, target - i);
+                curr.remove(curr.size() - 1);
+            }
         }
     }
+    
+    
 }
