d77a99e037134a828c5c599b9976329ac59fe8c5
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-12 22:09:40 +0530

    Time: 111 ms (83.41%), Space: 43.7 MB (76.04%) - LeetHub

--- 0447-number-of-boomerangs/0447-number-of-boomerangs.java
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.java
@@ -1,27 +1,28 @@
 class Solution {
     public int numberOfBoomerangs(int[][] points) {
         
-        int count=0;
-        int n=points.length;
-        
+        int n = points.length;
         Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
         
         for(int i=0; i<n; i++){
+            
             for(int j=0; j<n; j++){
-                if(i != j){
-                  int dist = ((points[i][0] - points[j][0]) * (points[i][0] - points[j][0])) + ((points[i][1] - points[j][1]) * (points[i][1] - points[j][1]));
                 
-                  if(!map.containsKey(dist)){
-                    map.put(dist, 1);
-                  }
-                  else{
-                    map.put(dist, map.get(dist) + 1);
-                  } 
+                if(i != j){
+                    int d = ((points[i][0] - points[j][0]) * (points[i][0] - points[j][0])) + ((points[i][1] - points[j][1]) * (points[i][1] - points[j][1]));
+                    
+                    if(!map.containsKey(d)){
+                        map.put(d, 1);
+                    }
+                    else{
+                        map.put(d, map.get(d) + 1);
+                    }
                 }
             }
             
             for(Integer num : map.values()){
-                count += num * (num-1);
+                count += num * (num - 1);
             }
             map.clear();
         }
