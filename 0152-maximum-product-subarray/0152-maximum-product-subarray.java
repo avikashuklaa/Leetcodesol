8d9c35c15f4364b275afc47fce9e057ec896c787
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-14 23:14:43 +0530

    Time: 2 ms (50.20%), Space: 43.3 MB (66.68%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,31 +1,29 @@
 class Solution {
     
-    // public void swap(int a, int b){
-    //     int temp=a;
-    //     a=b;
-    //     b=temp;
-    // }
+    
     public int maxProduct(int[] nums) {
-        int minsf=nums[0];
-        int maxsf=nums[0];
-        int ans=nums[0];
-        int max=nums[0];
-        int min=nums[0];
+       
+        int minsf = nums[0];
+        int maxsf = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        int ans = nums[0];
         int n=nums.length;
         
         for(int i=1; i<n; i++){
             if(nums[i]<0){
-                max=Math.max(nums[i], minsf*nums[i]);
-                min=Math.min(nums[i], maxsf*nums[i]);
+                max = Math.max(nums[i], minsf*nums[i]);
+                min = Math.min(nums[i], maxsf*nums[i]);
             }
             else{
-                max=Math.max(nums[i], maxsf*nums[i]);
-                min=Math.min(nums[i], minsf*nums[i]);
+                max = Math.max(nums[i], nums[i]*maxsf);
+                min = Math.min(nums[i], nums[i]*minsf);
             }
-            maxsf=max;
-            minsf=min;
-            ans=Math.max(ans, maxsf);
+            maxsf = max;
+            minsf = min;
+            ans = Math.max(ans, maxsf);
         }
+        
         return ans;
     }
 }
