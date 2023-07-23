4d4db85ccd515c446fe8ec49985faf4f89212463
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-23 22:54:01 +0530

    Time: 1 ms (99.96%), Space: 44 MB (67.69%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,33 +1,34 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         
-               
-        int m = triangle.size();
-        int n = triangle.get(triangle.size() - 1).size();
+        int n = triangle.size();
+        int m = triangle.get(triangle.size() - 1).size();
         
-        int[][] dp= new int[m][n];
+        int[][] dp = new int[n][m];
+        
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
         }
         
         return find(triangle, 0, 0, dp);
+       
     }
     
-    public static int find(List<List<Integer>> triangle, int i, int j, int[][] dp){
+    public static int find(List<List<Integer>> t, int i, int j, int[][] dp){
         
-        if(i == triangle.size() - 1){
-            return triangle.get(i).get(j);
+        if(i == t.size() - 1){
+            return t.get(i).get(j);
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        int left = find(triangle, i+1, j, dp) + triangle.get(i).get(j);
-        int right = find(triangle, i+1, j+1, dp) + triangle.get(i).get(j);
+        int left = find(t, i+1, j, dp) + t.get(i).get(j);
+        int right = find(t, i+1, j+1, dp) + t.get(i).get(j);
         
         return dp[i][j] = Math.min(left, right);
-        
-       
     }
+    
+    
 }
