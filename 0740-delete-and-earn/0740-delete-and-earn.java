ce2356cf09eda64c39cfb14114abfad5188f2874
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-23 07:37:10 +0530

    Time: 3 ms (86.47%), Space: 44.6 MB (16.16%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,9 +1,10 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         
+        int max = Integer.MIN_VALUE;
         
-        int max=Integer.MIN_VALUE;
         int[] count = new int[10001];
+        int[] dp = new int[10001];
         
         for(int num : nums){
             count[num]++;
@@ -12,16 +13,13 @@ class Solution {
             }
         }
         
-        int[] dp = new int[10001];
+        dp[1]=count[1];
+        dp[2] = Math.max(dp[1], count[2]*2);
         
-        dp[1] = count[1];
-        dp[2]= Math.max(dp[1], count[2]*2);
-        
-        for(int i=3; i<=max; i++){
-            dp[i] = Math.max(dp[i-1], dp[i-2] + count[i]*i);
+        for(int i=3; i<max+1; i++){
+            dp[i] = Math.max(dp[i-1], count[i]*i + dp[i-2]);
         }
         
         return dp[max];
-        
     }
 }
