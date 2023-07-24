02dac3d1475b5443819ea5fb84c9364d17d9228c
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-24 07:14:02 +0530

    Time: 2 ms (95.06%), Space: 44 MB (84.49%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -6,40 +6,42 @@ class Solution {
         
         Integer[][] dp = new Integer[n][n];
         
+       
         int ans = Integer.MAX_VALUE;
-        
         for(int i=0; i<n; i++){
             ans = Math.min(ans, find(matrix, dp, 0, i));
         }
         
         return ans;
+       
     }
     
-    public static int find(int[][] matrix, Integer[][] dp, int r, int c){
+    public static int find(int[][] mat, Integer[][] dp, int r, int c){
         
-        if(r<0 || r>=n || c<0 || c>=n){
+        if( r<0 || r>n-1 || c<0 || c>n-1){
             return Integer.MAX_VALUE;
         }
         
-        if(r==n-1){
-            return matrix[r][c];
+        if(r == n-1){
+            return mat[r][c];
         }
         
         if(dp[r][c] != null){
             return dp[r][c];
         }
         
-        int b = find(matrix, dp, r+1, c);
-        int bl = find(matrix, dp, r+1, c-1);
-        int br = find(matrix, dp, r+1, c+1);
+        int b=find(mat, dp, r+1, c);
+        int bl = find(mat, dp, r+1, c-1);
+        int br = find(mat, dp, r+1, c+1);
         
-        int min = Math.min(b, Math.min(bl, br));
+        int ans = Math.min(b, Math.min(bl, br));
         
-        dp[r][c] = matrix[r][c] + min;
+        dp[r][c] = ans + mat[r][c];
         
         return dp[r][c];
-        
     }
+    
+    
     
     
 }
