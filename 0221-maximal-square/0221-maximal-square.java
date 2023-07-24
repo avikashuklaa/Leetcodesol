3417737cc91495a4d5d791705b5be77208ab78a7
Avika Shukla <80634978+avikashuklaa@users.noreply.github.com>
2023-07-24 07:23:07 +0530

    Time: 5 ms (97.70%), Space: 54.5 MB (23.05%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,52 +1,44 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
         
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-            return 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] t = new int[m][n];
-        //top row
-        for (int i = 0; i < m; i++) 
-        {
-            t[i][0] = Character.getNumericValue(matrix[i][0]);
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int[][] dp = new int[n][m];
+        
+        for(int i=0; i<n; i++){
+            dp[i][0] = Character.getNumericValue(matrix[i][0]);
         }
-        //left column
-        for (int j = 0; j < n; j++) 
-        {
-            t[0][j] = Character.getNumericValue(matrix[0][j]);
+        
+        for(int j=0; j<m; j++){
+            dp[0][j] = Character.getNumericValue(matrix[0][j]);
         }
-        //cells inside
-        for (int i = 1; i < m; i++) 
-        {
-            for (int j = 1; j < n; j++) 
-            {
-                if (matrix[i][j] == '1') 
-                {
-                    int min = Math.min(t[i - 1][j], t[i - 1][j - 1]);
-                    min = Math.min(min,t[i][j - 1]);
-                    t[i][j] = min + 1;
-                } 
-                else 
-                {
-                    t[i][j] = 0;
+        
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] == '1'){
+                    int min = Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
+                    dp[i][j] = min + 1;
                 }
+                else{
+                    dp[i][j] = 0;
+                }
+                
             }
         }
+        
         int max = 0;
-        //get maximal length
-        for (int i = 0; i < m; i++) 
-        {
-            for (int j = 0; j < n; j++) 
-            {
-                if (t[i][j] > max) 
-                {
-                    max = t[i][j];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(dp[i][j] > max){
+                    max = dp[i][j];
                 }
             }
         }
+        
         return max * max;
-    }
+  }
 }
         
 //         int[][] dp = new int[matrix.length][matrix[0].length];
